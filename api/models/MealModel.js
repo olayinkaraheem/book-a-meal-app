196e7ba6901423b799996cc2b4b6db0091561dec
Raheem Kareem <olayinka.raheem16@gmail.com>
2019-02-18 17:29:34 +0100

    Revert "Revert 7 feature/endpoints"

---
+++ api/models/MealModel.js
@@ -0,0 +1,16 @@
+export default class Meal {
+  constructor() {
+    this.id = null;
+    this.name = null;
+    this.size = null;
+    this.price = null;
+    this.currency = null;
+    this.caterer_id = null;
+    this.active_today = null;
+    this.active = null;
+    this.rating = null;
+    this.updated_at = null;
+    this.updated_by = null;
+    this.created_at = null;
+  }
+}
